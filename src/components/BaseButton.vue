e4ee6b3b9473e5da259ed3fec9a7445c357545d8
DieuveilleSuthi <mdieuveillesuthi@gmail.com>
2023-10-16 14:41:54 +0200

    Ex 7: create BaseButton with primary color

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,58 @@
+<template>
+    <button
+      class="base-button"
+      :class="{ 'hovered': isHovered, 'focused': isFocused, 'disabled': isDisabled }"
+      @mouseenter="isHovered = true"
+      @mouseleave="isHovered = false"
+      @focus="isFocused = true"
+      @blur="isFocused = false"
+      :disabled="isDisabled"
+    >
+      <slot></slot>
+    </button>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        isHovered: false,
+        isFocused: false,
+      };
+    },
+    props: {
+      isDisabled: {
+        type: Boolean,
+        default: false,
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* BaseButton styles */
+  .base-button {
+    padding: 10px 20px;
+    border: none;
+    cursor: pointer;
+    background-color: green;
+    transition: background-color 0.3s, transform 0.1s;
+    margin : 10px;
+    padding: 10px;
+    border-radius: 8px
+  }
+  
+  .base-button.hovered {
+    background-color: #78c978; /* Background color on hover */
+  }
+  
+  .base-button.focused {
+    transform: scale(1.05); /* Scale effect on focus */
+  }
+  
+  .base-button.disabled {
+    background-color: #79dd9e; /* Disabled state background color */
+    cursor: not-allowed;
+  }
+  </style>
+  
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,12 +1,20 @@
 <template>
     <div class="corps">
-    <p>Here comes the content of the HomePage</p>
+         <p>Here comes the content of the HomePage</p>
+         <BaseButton>BaseButton with custom margin</BaseButton>
+         <BaseButton :is-disabled="true">BaseButton Disabled</BaseButton>
     </div>
 </template>
 
+<script>
+import BaseButton from './BaseButton.vue';
 
-
-
+export default {
+  components: {
+    BaseButton,
+  },
+};
+</script>
 
 <style scoped>
 .corps{
