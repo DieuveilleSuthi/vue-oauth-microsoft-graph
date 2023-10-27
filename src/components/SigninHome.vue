063c428d226a691b75e5a2cd0a40044f78bb5fd1
DieuveilleSuthi <mdieuveillesuthi@gmail.com>
2023-10-27 23:21:19 +0200

    Ex 1: add SigninButton

---
+++ src/components/SigninHome.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h1>{{ Country }}</h1>
+      <button @click="choisirPays">Choose</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ["pays"],
+    methods: {
+      choisirPays() {
+        this.$emit("choose", this.pays);
+      },
+    },
+  };
+  </script>
+  
